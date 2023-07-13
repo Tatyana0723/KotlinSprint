cea5d90d8e289235f6a761e7ad8880a1ed667d54
Tatyana0723 <mtl_80@list.ru>
2023-07-14 00:57:21 +0700

    Initial Commit

---
+++ src/main/kotlin/lesson_1_task_1.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thankText: String = "Спасибо за покупку! Мы свяжемся с Вами в ближайшее время."
+
+    println(numberOfOrders)
+    println(thankText)
+}

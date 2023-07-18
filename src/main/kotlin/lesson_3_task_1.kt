f8ed588324b8897e3ffb576c379e9b62f990a8a6
Tatyana0723 <mtl_80@list.ru>
2023-07-19 01:09:53 +0700

    KS-3-1 string from lesson 3 (print greeting)

---
+++ src/main/kotlin/lesson_3_task_1.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val userName = readLine()
+    var greeting = "добрый день!"
+
+    println("$userName, $greeting")
+
+    greeting = "добрый вечер!"
+
+    println("$userName, $greeting")
+
+
+}
